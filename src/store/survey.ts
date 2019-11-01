1fcb2cd4df8501c209767a579fb4e35cd341650d
ilya_savchuk <ilya.savchook@gmail.com>
2019-11-01 10:17:56 +0200

    Modify the survey's store for the DP survey

--- src/store/survey.ts
+++ src/store/survey.ts
@@ -1,96 +1,151 @@
 import { Module } from 'vuex'
 import { RootState, SurveyState } from '@/store'
-import { Section, SurveyInfo, CurrentSurveyData, SurveyUser } from '@/interfaces/SurveyInterfaces'
+import { Section, SurveyInfo, TakenSurveyData, NextDpSurveyPart, SurveyUserDto } from '@/interfaces/SurveyInterfaces'
 
 export interface SurveyState {
-  isCurrentSurveyInitiated: boolean
-  currentSurveyInfo: SurveyInfo | null
-  currentProductSurveyId: number | null
-  currentProductSurveyType: string | null
-  currentProductSurveySections: Section[]
-  currentProductSurveySection: Section | null
+  displayedBaseSurveyInfo: SurveyInfo | null
+
+  isTakenSurveyInitiated: boolean
+  isDpTakenSurvey: boolean
+
+  takenBaseSurveyInfo: SurveyInfo | null
+  takenSurveyId: number | null
+  takenSurveyType: string | null
+  takenSurveyUserId: number | null
+  takenSurveySections: Section[]
+  takenSurveySection: Section | null
   countCompletedSections: number
-  surveyUserInfo: SurveyUser | null
+
+  dpBaseSurveyInfo: SurveyInfo | null
+  dpSurveyId: number | null
+  dpSurveyUserId: number | null
 }
 
 const survey: Module<SurveyState, RootState> = {
   namespaced: true,
 
   state: {
-    isCurrentSurveyInitiated: false,
-    currentSurveyInfo: null,
-    currentProductSurveyId: null,
-    currentProductSurveyType: null,
-    currentProductSurveySections: [],
-    currentProductSurveySection: null,
+    displayedBaseSurveyInfo: null,
+
+    isTakenSurveyInitiated: false,
+    isDpTakenSurvey: false,
+
+    takenBaseSurveyInfo: null,
+    takenSurveyId: null,
+    takenSurveyType: null,
+    takenSurveyUserId: null,
+    takenSurveySections: [],
+    takenSurveySection: null,
     countCompletedSections: 0,
-    surveyUserInfo: null
+
+    dpBaseSurveyInfo: null,
+    dpSurveyId: null,
+    dpSurveyUserId: null,
   },
 
   getters: {
-    getCurrentSurveyInfo (state: SurveyState) : SurveyInfo | null {
-      return state.currentSurveyInfo
+    getDisplayedBaseSurveyInfo (state: SurveyState) : SurveyInfo | null {
+      return state.displayedBaseSurveyInfo
     },
-    getCurrentProductSurveyId (state: SurveyState) : number | null {
-      return state.currentProductSurveyId
+    getTakenSurveyId (state: SurveyState) : number | null {
+      return state.takenSurveyId
     },
-    getCurrentProductSurveyType (state: SurveyState) : string | null {
-      return state.currentProductSurveyType
+    getTakenSurveyType (state: SurveyState) : string | null {
+      return state.takenSurveyType
+    },
+    getTakenSurveyUserId (state: SurveyState) : number | null {
+      return state.takenSurveyUserId
+    },
+    getDpSurveyUserId (state: SurveyState) : number | null {
+      return state.dpSurveyUserId
+    },
+    getDpSurveyId (state: SurveyState) : number | null {
+      return state.dpSurveyId
     },
     getCurrentProductSurveySectionCount (state: SurveyState): number {
-      return state.currentProductSurveySections.length
+      return state.takenSurveySections.length
     },
-    getCurrentProductSurveySection (state: SurveyState) : Section | null {
-      return state.currentProductSurveySection
+    getNextTakenSurveySection (state: SurveyState) : Section | null {
+      return state.takenSurveySection
     },
     getCountCompletedSurveySection (state: SurveyState) : Number {
       return state.countCompletedSections
     },
-    isCurrentSurveyInitiated (state: SurveyState) : boolean {
-      return state.isCurrentSurveyInitiated
+    isTakenSurveyInitiated (state: SurveyState) : boolean {
+      return state.isTakenSurveyInitiated
+    },
+    isDpTakenSurvey (state: SurveyState) : boolean {
+      return state.isDpTakenSurvey
     },
     getNextSurveySectionNumber (state: SurveyState) : number | null {
-      return state.currentProductSurveySection && state.currentProductSurveySection.position != null
-        ? state.currentProductSurveySection.position + 1
+      return state.takenSurveySection && state.takenSurveySection.position != null
+        ? state.takenSurveySection.position + 1
         : null
     }
   },
 
   mutations: {
-    setCurrentSurveyProgress (state: SurveyState, progress: number): void {
+    setTakenSurveyData (state: SurveyState, surveyData: TakenSurveyData) : void {
+      state.takenBaseSurveyInfo = surveyData.surveyInfo
+      state.displayedBaseSurveyInfo = state.takenBaseSurveyInfo
+
+      if (state.takenSurveyType === 'discovery-process') {
+        state.isDpTakenSurvey = true
+        state.dpSurveyId = surveyData.productSurveyId
+        state.dpBaseSurveyInfo = state.takenBaseSurveyInfo
+      }
+
+      state.takenSurveyId = surveyData.productSurveyId
+      state.takenSurveyType = surveyData.productSurveyType
+      state.isTakenSurveyInitiated = true
+    },
+    setTakenSurveyUserId (state: SurveyState, surveyUserInfo: SurveyUserDto) : void {
+      state.takenSurveyUserId = surveyUserInfo.surveyUserId
+
+      if (surveyUserInfo.productSurveyType === 'discovery-process') {
+        state.dpSurveyUserId = surveyUserInfo.surveyUserId
+      }
+    },
+    useDpBaseSurveyInfo(state: SurveyState) : void {
+      state.displayedBaseSurveyInfo = state.dpBaseSurveyInfo
+    },
+    setCurrentSurveyProgress (state: SurveyState, progress: number) : void {
       state.countCompletedSections = progress
     },
-    setCurrentSurveyData (state: SurveyState, surveyData: CurrentSurveyData) : void {
-      state.currentSurveyInfo = surveyData.surveyInfo
-      state.currentProductSurveyId = surveyData.productSurveyId
-      state.currentProductSurveyType = surveyData.productSurveyType
-      state.surveyUserInfo = surveyData.surveyUserInfo
-      state.isCurrentSurveyInitiated = true
+    setTakenSurveySections (state: SurveyState, sections: Section[]) : void {
+      state.takenSurveySections = sections
+      state.takenSurveySection = sections[0] || null
     },
-    setCurrentSurveySections (state: SurveyState, sections: Section[]) : void {
-      state.currentProductSurveySections = sections
-      state.currentProductSurveySection = sections[0] || null
-    },
-    clearCurrentSurveyData (state: SurveyState) : void {
-      state.isCurrentSurveyInitiated = false
-      state.currentSurveyInfo = null
-      state.currentProductSurveyId = null
-      state.currentProductSurveyType = null
-      state.currentProductSurveySections = []
-      state.surveyUserInfo = null
-    },
+    clearTakenSurveyData (state: SurveyState) : void {
+      state.displayedBaseSurveyInfo = state.dpBaseSurveyInfo
 
+      state.takenBaseSurveyInfo = null
+      state.takenSurveyId = null
+      state.takenSurveyType = null
+      state.takenSurveyUserId = null
+      state.takenSurveySections = []
+      state.takenSurveySection = null
+    },
+    clearDpSurveyData (state: SurveyState) : void {
+      state.displayedBaseSurveyInfo = null
+      state.dpBaseSurveyInfo = null
+      state.isDpTakenSurvey = false
+
+      state.dpBaseSurveyInfo = null
+      state.dpSurveyId = null
+      state.dpSurveyUserId = null
+      state.countCompletedSections = 0
+    },
     setNextSurveySectionId (state: SurveyState, nextSectionId: number) : void {
-      const nextSection: Section | undefined = state.currentProductSurveySections.find((section: Section) => {
+      const nextSection: Section | undefined = state.takenSurveySections.find((section: Section) => {
         return section.id === nextSectionId
       })
 
-      state.currentProductSurveySection = nextSection || null
+      state.takenSurveySection = nextSection || null
     },
-
     addOneCompletedSection (state: SurveyState) : void {
       state.countCompletedSections++
-    }
+    },
   }
 }
 
