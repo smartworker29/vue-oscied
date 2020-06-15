171d57956408ffe78e334cfc0f97ef6bfe6e3f91
ilya_savchuk <ilya.savchook@gmail.com>
2020-06-15 17:36:25 +0300

    add dashboard's interfaces

---
+++ src/interfaces/DashboardInterface.ts
@@ -0,0 +1,33 @@
+export interface DashboardBaseItem {
+  timeCreated: string
+  timeCompleted: string | null
+}
+
+export interface DashboardSurveyItem extends DashboardBaseItem {
+  surveyUserId: number
+  surveyProduct: {
+    id: number
+    survey: {
+      id: number
+      title: string
+      accessCode: string
+    }
+  }
+}
+
+export interface DashboardIcoachItem  extends DashboardBaseItem {
+  id: number
+  icoachCourse: {
+    id: number
+    title: string
+    accessCode: string
+  }
+}
+
+export interface DashboardResponse {
+  eq: DashboardSurveyItem[]
+  values: DashboardSurveyItem[]
+  behaviours: DashboardSurveyItem[]
+  dp: DashboardSurveyItem[]
+  icoach: DashboardIcoachItem[]
+}
