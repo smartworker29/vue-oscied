2f7196e14b90c366e7df2b3a7567af4fbac02257
ilya_savchuk <ilya.savchook@gmail.com>
2020-06-16 17:53:52 +0300

    add MainLogo state

--- src/store.ts
+++ src/store.ts
@@ -4,23 +4,27 @@ import Vuex from 'vuex'
 import user, { UserState } from '@/store/user'
 import survey, { SurveyState } from '@/store/survey'
 import icoach, { IcoachState } from '@/store/icoach'
+import mainLogo , { MainLogoState } from '@/store/main-logo'
 
 Vue.use(Vuex)
 
 export { UserState } from '@/store/user'
 export { SurveyState } from '@/store/survey'
 export { IcoachState } from '@/store/icoach'
+export { MainLogoState } from '@/store/main-logo'
 
 export interface RootState {
   user: UserState
   survey: SurveyState
   icoach: IcoachState
+  mainLogo: MainLogoState
 }
 
 export default new Vuex.Store<RootState>({
   modules: {
     user,
     survey,
-    icoach
+    icoach,
+    mainLogo
   }
 })
