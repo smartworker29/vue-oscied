0c17880b8d5ac33f6820437f236dcb15b47fa95c
ilya_savchuk <ilya.savchook@gmail.com>
2020-06-16 17:55:27 +0300

    rename logos component

---
+++ src/components/common/layout/MainLogos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="survey-logos" v-if="items && items.length > 0">
+    <div class="survey-logo" v-for="(logo, index) in items" :key="index">
+      <img :src="logo.fileURL">
+    </div>
+  </div>
+  <img v-else class="navbar-logo" :src="require('@/assets/logo-ccr-black.svg')" />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { Getter } from 'vuex-class'
+import { Image } from '@/interfaces/GeneralInterfaces'
+
+@Component({ name: 'MainLogos' })
+export default class MainLogos extends Vue {
+  @Getter('mainLogo/getLogos')
+  items!: Image[]
+}
+</script>
